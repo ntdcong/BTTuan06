36579a22567f3dbd3163e74f8f3d67487152f8ae
Công Nguyễn <duycong2580@gmail.com>
2024-10-03 10:12:39 +0700

    Add project files.

---
+++ TinhToanCongTru/2180609349 - 2180609353.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TinhToanCongTru
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(58, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(98, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(213, 60);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(98, 27);
+            textBox2.TabIndex = 2;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(174, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(19, 20);
+            label2.TabIndex = 4;
+            label2.Text = "+";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(347, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 20);
+            label3.TabIndex = 5;
+            label3.Text = "=";
+            label3.Click += label3_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(40, 32);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(558, 158);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Phép cộng - Duy Công làm";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(395, 111);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 26);
+            button1.TabIndex = 6;
+            button1.Text = "Tính";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "2180609349 - 2180609353";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox1;
+        private Button button1;
+    }
+}
---
+++ TinhToanCongTru/2180609349 - 2180609353.cs
@@ -0,0 +1,65 @@
+﻿namespace TinhToanCongTru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lấy giá trị từ các TextBox
+                double so1 = Convert.ToDouble(textBox1.Text);
+                double so2 = Convert.ToDouble(textBox2.Text);
+
+                // Thực hiện các phép tính
+                double tong = so1 + so2;
+
+                // Hiển thị kết quả
+                label3.Text = $"Tổng: {tong}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vui lòng nhập số hợp lệ.");
+            }
+        }
+    }
+}
